60c2e537d242f69e4f8d0ee0c6fd0b9c35abfa11
Nogic <24802730+nogic1008@users.noreply.github.com>
2019-11-19 14:20:26 +0900

    Add Sample Program & Test

--- Sample.Core/Program.cs
+++ Sample.Core/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Linq;
 
 namespace Sample.Core
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        private static void Main()
+            => Console.WriteLine(
+                string.Join(", ", Enumerable.Range(1, 20).Select(i => i.ToFizzBuzzFormat()))
+            );
+    }
+
+    public static class FizzBuzz
+    {
+        public static string ToFizzBuzzFormat(this int i)
+            => i % 15 == 0 ? "Fizz Buzz"
+            : i % 5 == 0 ? "Buzz"
+            : i % 3 == 0 ? "Fizz"
+            : i.ToString();
     }
 }
