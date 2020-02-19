e561ebbbe0bc87bfbe70fabb3465272bcb27ca0a
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-02-19 16:41:59 +0900

    refactor: split FizzBuzz class

--- Sample.Core/Program.cs
+++ Sample.Core/Program.cs
@@ -10,13 +10,4 @@ namespace Sample.Core
                 string.Join(", ", Enumerable.Range(1, 20).Select(i => i.ToFizzBuzzFormat()))
             );
     }
-
-    public static class FizzBuzz
-    {
-        public static string ToFizzBuzzFormat(this int i)
-            => i % 15 == 0 ? "Fizz Buzz"
-            : i % 5 == 0 ? "Buzz"
-            : i % 3 == 0 ? "Fizz"
-            : i.ToString();
-    }
 }
