354256ab1b733b40995d33cac7c68ed922d50423
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-04-14 09:31:09 +0900

    feat: init project

---
+++ src/Sample.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
