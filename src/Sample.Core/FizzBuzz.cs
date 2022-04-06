4b19df7d3aa69b88ff17813415e8b5f2415b850a
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-04-06 11:01:38 +0900

    style(csharp): use File Scoped Namespaces
    https://docs.microsoft.com/dotnet/csharp/language-reference/proposals/csharp-10.0/file-scoped-namespaces

--- src/Sample.Core/FizzBuzz.cs
+++ src/Sample.Core/FizzBuzz.cs
@@ -1,11 +1,10 @@
-namespace Sample.Core
+namespace Sample.Core;
+
+public static class FizzBuzz
 {
-    public static class FizzBuzz
-    {
-        public static string ToFizzBuzzFormat(this int i)
-            => i % 15 == 0 ? "Fizz Buzz"
-            : i % 5 == 0 ? "Buzz"
-            : i % 3 == 0 ? "Fizz"
-            : i.ToString();
-    }
+    public static string ToFizzBuzzFormat(this int i)
+        => i % 15 == 0 ? "Fizz Buzz"
+        : i % 5 == 0 ? "Buzz"
+        : i % 3 == 0 ? "Fizz"
+        : i.ToString();
 }
