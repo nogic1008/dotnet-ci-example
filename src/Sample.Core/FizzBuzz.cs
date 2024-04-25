4bc4eb914216c890990b1af9260df8c17ce35349
nogic1008 <24802730+nogic1008@users.noreply.github.com>
2024-04-25 23:29:47 +0900

    docs: add xml documentation comment

--- src/Sample.Core/FizzBuzz.cs
+++ src/Sample.Core/FizzBuzz.cs
@@ -1,7 +1,20 @@
 namespace Sample.Core;
 
+/// <summary>
+/// Extension methods for int.
+/// </summary>
 public static class FizzBuzz
 {
+    /// <summary>
+    /// Convert the integer to FizzBuzz format.
+    /// </summary>
+    /// <param name="i">target integer</param>
+    /// <returns>
+    /// "Fizz Buzz" if the integer is divisible by 15,
+    /// "Buzz" if the integer is divisible by 5,
+    /// "Fizz" if the integer is divisible by 3,
+    /// otherwise the integer itself.
+    /// </returns>
     public static string ToFizzBuzzFormat(this int i)
         => i % 15 == 0 ? "Fizz Buzz"
         : i % 5 == 0 ? "Buzz"
--- test/Sample.Test/FizzBuzz.Test.cs
+++ test/Sample.Test/FizzBuzz.Test.cs
@@ -3,33 +3,52 @@ using static Sample.Core.FizzBuzz;
 
 namespace Sample.Test;
 
-public class FizzBuzzTest
+/// <summary>
+/// Unit test for <see cref="Core.FizzBuzz"/>.
+/// </summary>
+public sealed class FizzBuzzTest
 {
+    /// <summary>
+    /// When <paramref name="i"/> is not devided by 3 and 5, <see cref="Core.FizzBuzz.ToFizzBuzzFormat"/> should return the number as string.
+    /// </summary>
+    /// <param name="i">input</param>
     [Theory]
     [InlineData(1)]
     [InlineData(2)]
     [InlineData(-1)]
-    public void ToFizzBuzzFormat_Returns_NumberString(int i)
+    public void When_No_Devided_3or5_ToFizzBuzzFormat_Returns_NumberString(int i)
         => Assert.Equal(i.ToString(), i.ToFizzBuzzFormat());
 
+    /// <summary>
+    /// When <paramref name="i"/> is devided by 3, <see cref="Core.FizzBuzz.ToFizzBuzzFormat"/> should return "Fizz".
+    /// </summary>
+    /// <param name="i">input</param>
     [Theory]
     [InlineData(3)]
     [InlineData(-3)]
     [InlineData(123456789)]
-    public void ToFizzBuzzFormat_Returns_Fizz(int i)
+    public void When_Devided_3_ToFizzBuzzFormat_Returns_Fizz(int i)
         => Assert.Equal("Fizz", i.ToFizzBuzzFormat());
 
+    /// <summary>
+    /// When <paramref name="i"/> is devided by 5, <see cref="Core.FizzBuzz.ToFizzBuzzFormat"/> should return "Buzz".
+    /// </summary>
+    /// <param name="i">input</param>
     [Theory]
     [InlineData(5)]
     [InlineData(-10)]
     [InlineData(2000)]
-    public void ToFizzBuzzFormat_Returns_Buzz(int i)
+    public void When_Devided_5_ToFizzBuzzFormat_Returns_Buzz(int i)
         => Assert.Equal("Buzz", i.ToFizzBuzzFormat());
 
+    /// <summary>
+    /// When <paramref name="i"/> is devided by 15, <see cref="Core.FizzBuzz.ToFizzBuzzFormat"/> should return "Fizz Buzz".
+    /// </summary>
+    /// <param name="i">input</param>
     [Theory]
     [InlineData(0)]
     [InlineData(15)]
     [InlineData(-30)]
-    public void ToFizzBuzzFormat_Returns_FizzBuzz(int i)
+    public void When_Devided_15_ToFizzBuzzFormat_Returns_FizzBuzz(int i)
         => Assert.Equal("Fizz Buzz", i.ToFizzBuzzFormat());
 }
