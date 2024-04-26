e312cc7fc72e95065047b2f84ba390d3768c0703
nogic1008 <24802730+nogic1008@users.noreply.github.com>
2024-04-26 10:56:46 +0900

    feat: use MSTest SDK

--- test/Sample.Test/FizzBuzz.Test.cs
+++ test/Sample.Test/FizzBuzz.Test.cs
@@ -1,54 +1,64 @@
-using Xunit;
-using static Sample.Core.FizzBuzz;
+using Sample.Core;
 
 namespace Sample.Test;
 
 /// <summary>
-/// Unit test for <see cref="Core.FizzBuzz"/>.
+/// Unit test for <see cref="FizzBuzz"/>.
 /// </summary>
+[TestClass]
 public sealed class FizzBuzzTest
 {
     /// <summary>
-    /// When <paramref name="i"/> is not devided by 3 and 5, <see cref="Core.FizzBuzz.ToFizzBuzzFormat"/> should return the number as string.
+    /// Generate values that satisfy the <paramref name="predicate"/>.
     /// </summary>
-    /// <param name="i">input</param>
-    [Theory]
-    [InlineData(1)]
-    [InlineData(2)]
-    [InlineData(-1)]
-    public void When_No_Devided_3or5_ToFizzBuzzFormat_Returns_NumberString(int i)
-        => Assert.Equal(i.ToString(), i.ToFizzBuzzFormat());
+    /// <param name="predicate">
+    /// <inheritdoc cref="Enumerable.Where{TSource}(IEnumerable{TSource}, Func{TSource, bool})" path="/param[@name='predicate']"/>
+    /// </param>
+    private static int[] GenerateValues(Func<int, bool> predicate)
+    {
+        const int count = 10;
+        int[] values = new int[count];
+        for (int i = 0; i < values.Length; i++)
+        {
+            int value;
+            do
+            {
+                value = FixtureFactory.Create<int>();
+            } while (!predicate(value));
+            values[i] = value;
+        }
+        return values;
+    }
 
     /// <summary>
-    /// When <paramref name="i"/> is devided by 3, <see cref="Core.FizzBuzz.ToFizzBuzzFormat"/> should return "Fizz".
+    /// When number is not divisible by 3 and 5, <see cref="FizzBuzz.ToFizzBuzzFormat"/> returns same value as <see cref="int.ToString()"/>.
     /// </summary>
-    /// <param name="i">input</param>
-    [Theory]
-    [InlineData(3)]
-    [InlineData(-3)]
-    [InlineData(123456789)]
-    public void When_Devided_3_ToFizzBuzzFormat_Returns_Fizz(int i)
-        => Assert.Equal("Fizz", i.ToFizzBuzzFormat());
+    [TestMethod($"{nameof(FizzBuzz)} > {nameof(FizzBuzz.ToFizzBuzzFormat)}(<not a multiple of 3 and 5>) > returns same value as int.{nameof(int.ToString)}()")]
+    public void When_Not_Devisible_By_3_And_5_ToFizzBuzzFormat_Returns_ToString()
+        => GenerateValues(static i => i % 3 != 0 && i % 5 != 0)
+            .Should().AllSatisfy(static i => i.ToFizzBuzzFormat().Should().Be(i.ToString()));
 
     /// <summary>
-    /// When <paramref name="i"/> is devided by 5, <see cref="Core.FizzBuzz.ToFizzBuzzFormat"/> should return "Buzz".
+    /// When number is divisible by 3, <see cref="FizzBuzz.ToFizzBuzzFormat"/> returns "Fizz".
     /// </summary>
-    /// <param name="i">input</param>
-    [Theory]
-    [InlineData(5)]
-    [InlineData(-10)]
-    [InlineData(2000)]
-    public void When_Devided_5_ToFizzBuzzFormat_Returns_Buzz(int i)
-        => Assert.Equal("Buzz", i.ToFizzBuzzFormat());
+    [TestMethod($"{nameof(FizzBuzz)} > {nameof(FizzBuzz.ToFizzBuzzFormat)}(<multiple of 3>) > returns \"Fizz\"")]
+    public void When_Devisible_By_3_ToFizzBuzzFormat_Returns_Fizz()
+        => GenerateValues(static i => i % 3 == 0 && i % 5 != 0)
+            .Should().AllSatisfy(static i => i.ToFizzBuzzFormat().Should().Be("Fizz"));
 
     /// <summary>
-    /// When <paramref name="i"/> is devided by 15, <see cref="Core.FizzBuzz.ToFizzBuzzFormat"/> should return "Fizz Buzz".
+    /// When number is divisible by 5, <see cref="FizzBuzz.ToFizzBuzzFormat"/> returns "Buzz".
     /// </summary>
-    /// <param name="i">input</param>
-    [Theory]
-    [InlineData(0)]
-    [InlineData(15)]
-    [InlineData(-30)]
-    public void When_Devided_15_ToFizzBuzzFormat_Returns_FizzBuzz(int i)
-        => Assert.Equal("Fizz Buzz", i.ToFizzBuzzFormat());
+    [TestMethod($"{nameof(FizzBuzz)} > {nameof(FizzBuzz.ToFizzBuzzFormat)}(<multiple of 5>) > returns \"Buzz\"")]
+    public void When_Devisible_By_5_ToFizzBuzzFormat_Returns_Buzz()
+        => GenerateValues(static i => i % 3 != 0 && i % 5 == 0)
+            .Should().AllSatisfy(static i => i.ToFizzBuzzFormat().Should().Be("Buzz"));
+
+    /// <summary>
+    /// When number is divisible by 15, <see cref="FizzBuzz.ToFizzBuzzFormat"/> returns "Fizz Buzz".
+    /// </summary>
+    [TestMethod($"{nameof(FizzBuzz)} > {nameof(FizzBuzz.ToFizzBuzzFormat)}(<multiple of 15>) > returns \"Fizz Buzz\"")]
+    public void When_Devisible_By_15_ToFizzBuzzFormat_Returns_FizzBuzz()
+        => GenerateValues(static i => i % 3 == 0 && i % 5 == 0)
+            .Should().AllSatisfy(static i => i.ToFizzBuzzFormat().Should().Be("Fizz Buzz"));
 }
